33fa7041ef2f239c3616d2823d8a11dbf23ea61f
Ichiro Yamamoto <yichi@otakukingdom.com>
2014-05-12 14:34:17 -0600

    AR relations

--- app/models/item_set.rb
+++ app/models/item_set.rb
@@ -1,2 +1,5 @@
 class ItemSet < ActiveRecord::Base
+  has_many :item_set_blocks, :dependent => :delete_all
+  belongs_to :user
+
 end
--- app/models/item_set_block.rb
+++ app/models/item_set_block.rb
@@ -1,2 +1,3 @@
 class ItemSetBlock < ActiveRecord::Base
+  has_many :block_items, :dependent => :delete_all
 end
