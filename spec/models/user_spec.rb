e73034a198ee67932b2391fd556739ffa2d0f1c0
Ichiro Yamamoto <yichi@otakukingdom.com>
2014-05-17 03:38:50 -0600

    another test case for authentication

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,8 @@ describe User do
     @user.email = "Ahri@example.com"
 
     @save_result = @user.save
+
+    @user2 = FactoryGirl.create(:user, :username => "longPassworduser", :password => "pDAxf6uut5mTSkO5ROR5Z7FELA9bLj6l6E")
   end
 
   it "should be able to create the user record" do
@@ -17,6 +19,11 @@ describe User do
   it "should be able to authenticate the user with correct credentials" do
     user_record = User.authenticate("Ahri", "f0xfire")
     user_record.username.should == "Ahri"
+  end
+
+  it "should be able to authenticate long password user with correct credentials" do
+    user_record = User.authenticate("longPassworduser", "pDAxf6uut5mTSkO5ROR5Z7FELA9bLj6l6E")
+    user_record.username.should == "longPassworduser"
   end
 
   it "should not be able to authenticate the user with incorrect credentials" do
